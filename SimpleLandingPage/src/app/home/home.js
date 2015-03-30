8b3131e8d934dda9b53685e72d7a47f41ac2586a
daihuaye <daihua.ye@gmail.com>
2015-03-29 21:21:01 -0700

    remove comments in home controller

--- SimpleLandingPage/src/app/home/home.js
+++ SimpleLandingPage/src/app/home/home.js
@@ -1,21 +1,6 @@
-/**
- * Each section of the site has its own module. It probably also has
- * submodules, though this boilerplate is too simple to demonstrate it. Within
- * 'src/app/home', however, could exist several additional folders representing
- * additional modules that would then be listed as dependencies of this one.
- * For example, a 'note' section could have the submodules 'note.create',
- * 'note.delete', 'note.edit', etc.
- *
- * Regardless, so long as dependencies are managed correctly, the build process
- * will automatically take take of the rest.
- */
 (function(module) {
 
-    // As you add controllers to a module and they grow in size, feel free to place them in their own files.
-    //  Let each module grow organically, adding appropriate organization and sub-folders as needed.
     module.controller('HomeController', function () {
-        // The top section of a controller should be lean and make it easy to see the "signature" of the controller
-        //  at a glance.  All function definitions should be contained lower down.
         var model = this,
             submitted = false;
         model.emails = ['one', 'two', 'three'];
@@ -46,5 +31,4 @@
 
     });
 
-// The name of the module, followed by its dependencies (at the bottom to facilitate enclosure)
 }(angular.module("SimpleLandingPage.home")));
